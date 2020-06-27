895157a8514072f7ec4604e6fde332fd8dcaf1f8
Pratik Shivaraikar <pratik.shivaraikar@gmail.com>
2020-06-28 01:03:55 +0530

    Create bolt bucket

--- db/tasks.go
+++ db/tasks.go
@@ -13,28 +13,32 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-package main
+package db
 
 import (
-	"fmt"
-	"os"
-	"path/filepath"
+	"time"
 
-	"github.com/mitchellh/go-homedir"
-	"github.com/pratikms/taskx-cli/cmd"
-	"github.com/pratikms/taskx-cli/db"
+	"github.com/boltdb/bolt"
 )
 
-func must(err error) {
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
+var taskBucket = []byte("tasks")
+var db *bolt.DB
+
+// Task represents the structured info that will be stored in the DB
+type Task struct {
+	Key   int
+	Value string
 }
 
-func main() {
-	home, _ := homedir.Dir()
-	dbPath := filepath.Join(home, "tasks.db")
-	must(db.Init(dbPath))
-	must(cmd.RootCmd.Execute())
+// Init represents the exported function that will accept the path at which the DB is to be stored
+func Init(dbPath string) error {
+	var err error
+	db, err = bolt.Open(dbPath, 0600, &bolt.Options{Timeout: 1 * time.Second})
+	if err != nil {
+		return err
+	}
+	return db.Update(func(tx *bolt.Tx) error {
+		_, err := tx.CreateBucketIfNotExists(taskBucket)
+		return err
+	})
 }
