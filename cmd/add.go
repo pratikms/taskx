07fe5d742d47a495d7624a3b754f16d9bd769fb1
Pratik Shivaraikar <pratik.shivaraikar@gmail.com>
2020-06-28 15:08:10 +0530

    Add support for 'add' option

--- cmd/add.go
+++ cmd/add.go
@@ -17,7 +17,10 @@ package cmd
 
 import (
 	"fmt"
+	"os"
 	"strings"
+
+	"github.com/pratikms/taskx-cli/db"
 
 	"github.com/spf13/cobra"
 )
@@ -27,6 +30,11 @@ var addCmd = &cobra.Command{
 	Short: "Adds a task to your task list",
 	Run: func(cmd *cobra.Command, args []string) {
 		task := strings.Join(args, " ")
+		_, err := db.CreateTask(task)
+		if err != nil {
+			fmt.Println("Something went wrong: ", err.Error())
+			os.Exit(1)
+		}
 		fmt.Printf("Added \"%s\" to your task list\n", task)
 	},
 }
