2004a3623e4dee3abfdea5cfe4fbad46a6b6d60d
Pratik Shivaraikar <pratik.shivaraikar@gmail.com>
2020-06-28 13:21:10 +0530

    Store tasks

--- db/tasks.go
+++ db/tasks.go
@@ -16,6 +16,7 @@ limitations under the License.
 package db
 
 import (
+	"encoding/binary"
 	"time"
 
 	"github.com/boltdb/bolt"
@@ -41,4 +42,30 @@ func Init(dbPath string) error {
 		_, err := tx.CreateBucketIfNotExists(taskBucket)
 		return err
 	})
+}
+
+func itob(v int) []byte {
+	b := make([]byte, 8)
+	binary.BigEndian.PutUint64(b, uint64(v))
+	return b
+}
+
+func btoi(b []byte) int {
+	return int(binary.BigEndian.Uint64(b))
+}
+
+// CreateTask stores the task
+func CreateTask(task string) (int, error) {
+	var id int
+	err := db.Update(func(tx *bolt.Tx) error {
+		b := tx.Bucket(taskBucket)
+		id64, _ := b.NextSequence()
+		id = int(id64)
+		key := itob(int(id64))
+		return b.Put(key, []byte(task))
+	})
+	if err != nil {
+		return -1, err
+	}
+	return id, nil
 }
