dc7bbdfc891ea773d863f515bbdba9aa977e2510
Pratik Shivaraikar <pratik.shivaraikar@gmail.com>
2020-06-28 13:36:13 +0530

    Read all tasks

--- db/tasks.go
+++ db/tasks.go
@@ -69,3 +69,23 @@ func CreateTask(task string) (int, error) {
 	}
 	return id, nil
 }
+
+// AllTasks will read tasks from bolt DB
+func AllTasks() ([]Task, error) {
+	var tasks []Task
+	err := db.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket(taskBucket)
+		c := b.Cursor()
+		for k, v := c.First(); k != nil; k, v = c.Next() {
+			tasks = append(tasks, Task{
+				Key:   btoi(k),
+				Value: string(v),
+			})
+		}
+		return nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	return tasks, nil
+}
